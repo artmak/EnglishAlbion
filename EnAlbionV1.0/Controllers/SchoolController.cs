68aaa6f5d2820988d9b3c01e48345754bcecacff
Vadym <vadymmakarenko@hotmail.com>
2013-09-06 09:55:12 +0300

    End off

---
+++ EnAlbionV1.0/Controllers/SchoolController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnAlbionV1._0.Controllers
+{
+    public class SchoolController : Controller
+    {
+        //
+        // GET: /School/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult English()
+        {
+            return View();
+        }
+
+        public ActionResult English_business_rates()
+        {
+            return View();
+        }
+    }
+}
