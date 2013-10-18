128fc532b9d66c94d5230606b462d77e6ff14885
Vadym <vadymmakarenko@hotmail.com>
2013-10-18 15:14:33 +0300

    ...

---
+++ EnAlbionV1.0/Controllers/OtherController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnAlbionV1._0.Controllers
+{
+    public class OtherController : Controller
+    {
+        //
+        // GET: /Other/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Article()
+        {
+            return View();
+        }
+
+        public ActionResult Books()
+        {
+            return View();
+        }
+        public ActionResult Video()
+        {
+            return View();
+        }
+        public ActionResult Audio()
+        {
+            return View();
+        }
+
+    }
+}
