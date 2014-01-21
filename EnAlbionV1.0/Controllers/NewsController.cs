c28bb93f2b84a82012706abb80e06d3b50a773e6
Vadym <dmhouse.dev@gmail.com>
2014-01-21 17:25:10 +0200

    Add
    Акция – декабрь месяц подарков –убрать
    Уже!!! ДЕКАБРЬ - МЕСЯЦ ПОДАРКОВ
    На главной странице под кнопками курс выходного дня
    Сделать кнопку- УЧИ ЯЗЫК ДЕШЕВЛЕ ! При нажатии можно будет попасть на
    акции,
    Акция- Получи скидку 10 %+10 %
    Порекомендуй учебный центр Albion знакомому или другу и Вы оба получите
    скидку по 10 % на месяц обучения.
    Чтобы стать участником акции знакомый должен назвать Ваше полное имя
    
    Убрать снежинки.
    
    Новости
    Новости
    НОВЫЙ УЧЕБНЫЙ СЕМЕСТР!!!
    Мы поздравляем Вас с новым учебным годом и рады сообщить, что языковая
    школа Green Apple объявляет набор на изучение: английского, немецкого,
    французского,языков в группах или индивидуально!
    
    ВСЕ УРОВНИ!
    
    Занятия проходят в вечернее или в дневное время!
    
    Напоминаем, что для студентов нашей школы по субботам разговорные клубы
    абсолютно БЕСПЛАТНО!
    
    Поторопись узнать о системе СКИДОК!
    ЭКОНОМЬ УЖЕ СЕЙЧАС!
    Звони и записывайся!
    
    При нажатии системе скидок-------- попадать на скидки

--- EnAlbionV1.0/Controllers/NewsController.cs
+++ EnAlbionV1.0/Controllers/NewsController.cs
@@ -19,6 +19,10 @@ namespace EnAlbionV1._0.Controllers
         {
             return View();
         }
+        public ActionResult Discounts()
+        {
+            return View();
+        }
 
     }
 }
